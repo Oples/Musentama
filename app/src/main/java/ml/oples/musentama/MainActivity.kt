a380f36ba4ac5acd96d3c27832874ebe255672bd
oples <oples.treasure@gmail.com>
2021-11-06 18:19:20 +0100

    add Options menu

--- app/src/main/java/ml/oples/musentama/MainActivity.kt
+++ app/src/main/java/ml/oples/musentama/MainActivity.kt
@@ -2,10 +2,25 @@ package ml.oples.musentama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_dev_menu -> Toast.makeText(this, "Help", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
